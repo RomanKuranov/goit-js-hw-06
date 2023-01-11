2c3dc6482f8e5e89dccd4f17611cf4d73d32c9ea
RomanKuranov <romankuranov93@gmail.com>
2023-01-11 21:02:23 +0200

    task 01,02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+const ingredientsEl = document.querySelector('#ingredients');
+const newLiEl = ingredients.map(ingredient => {
+  const newLi = document.createElement('li');
+  newLi.classList.add('item');
+  newLi.textContent = ingredient;
+  return newLi;
+});
+ingredientsEl.append(...newLiEl);
+console.log(ingredientsEl);

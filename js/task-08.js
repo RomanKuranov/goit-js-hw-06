b0617db84e58e7c3ca41fded78f5667a161329a7
RomanKuranov <romankuranov93@gmail.com>
2023-01-15 21:53:38 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector('.login-form');
 
+
+const onFormELSubmit = (event)=> {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+
+      if(email.value === "" || password.value === "") {
+        return alert("Все поля должны быть заполнены");
+      };
+
+     const formData = {
+        email,
+        password,
+     };
+     
+      event.currentTarget.reset();
+};
+formEl.addEventListener( 'submit', onFormELSubmit);

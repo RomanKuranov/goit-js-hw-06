0b9a9d03fb985f36f182e3f0543eda70c33a3ee6
RomanKuranov <romankuranov93@gmail.com>
2023-01-15 22:23:19 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  spanEl:document.querySelector('.color'),
+  buttonEl:document.querySelector('.change-color'),
+  bodyEl:document.querySelector('body'),
+}
+
+const onButtonClick = (event) => {
+  refs.spanEl.textContent = getRandomHexColor();
+  refs.bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+
+refs.buttonEl.addEventListener("click", onButtonClick);
